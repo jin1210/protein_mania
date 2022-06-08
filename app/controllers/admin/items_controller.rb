ef926bd5741b1b3682d28f55530316ae95f1873c
jin1210 <jinj86530@gmail.com>
2022-06-08 15:29:24 +0000

    [update]管理者画面の編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,38 @@
 class Admin::ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "successfully"
+      redirect_to admin_item(@item.id)
+    else
+     flash[:notice] = "unsuccessful"
+     render :new
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      flash[:notice] = "successfully"
+      redirect_to admin_item(@item.id)
+    else
+      flash[:notice] = "unsuccessful"
+      render :edit
+    end
+  end
+
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,27 @@
 class Admin::UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      flash[:notice] = "successfully"
+      redirect_to admin_user(@user.id)
+    else
+      flash[:notice] = "unsuccessful"
+      render :edit
+    end
+  end
+  
 end
