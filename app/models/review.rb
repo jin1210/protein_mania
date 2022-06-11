54ea734afceefd71d371f7e274fd0ed7cb48d1f1
jin1210 <jinj86530@gmail.com>
2022-06-11 14:36:52 +0000

    [update]public画面の更新

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,8 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :item
 
-  enum ease: { poor: 0, good: 1, excellent: 2 }
-  enum taste: { poor: 0, good: 1, excellent: 2 }
+  enum ease: { ease_poor: 0, ease_good: 1, ease_excellent: 2 }
+  enum taste: { taste_poor: 0, taste_good: 1, taste_excellent: 2 }
   enum purpose: { muscle: 0, diet: 1, others: 2 }
 
 end
